4af62f97647f25a2a262a21e4db903ae3742f88e
yashmishracode <yashmishrawork9@gmail.com>
2025-01-23 15:50:43 +0530

    migration and migrate

--- scholara/scholaraApp/models.py
+++ scholara/scholaraApp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Signup(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+
+
+class Login(models.Model):
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
--- scholara/scholaraApp/views.py
+++ scholara/scholaraApp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from scholaraApp.models import Signup, Login
 
 # Create your views here.
+
+# def index(request):
